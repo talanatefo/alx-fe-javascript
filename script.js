1c6a1e99a474e6c7591e74481fa6a99aacad3ba4
talana tefo <talanatefo@gmail.com>
2025-03-15 09:55:14 +0200

    added feat for new quote

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let newQuote = document.getElementById("newQuote");
 let quoteDisplay = document.getElementById("quoteDisplay");
 
+let newQuoteText = document.getElementById("newQuoteText");
+let newQuoteCategory = document.getElementById("newQuoteCategory");
+
 let quotes = [
   { text: "first quote", category: "motivation" },
   { text: "second quote", category: "spiritual" },
@@ -8,6 +11,22 @@ let quotes = [
   { text: "fourth quote", category: "health" },
   { text: "fifth quote", category: "motivation" },
 ];
+
+function showRandomQuote() {
+  let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  quoteDisplay.innerHTML = randomQuote.text + " " + randomQuote.category;
+}
+
+function addQuote() {
+  let newQuoteTextValue = newQuoteText.value.trim();
+  let newQuoteCategoryValue = newQuoteCategory.value.trim();
+
+  if (newQuoteTextValue && newQuoteCategoryValue) {
+    quotes.push({ text: newQuoteTextValue, category: newQuoteCategoryValue });
+  } else {
+    alert("Please fill in both fields (quote and category).");
+  }
+}
 
 function showRandomQuote() {
   let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
